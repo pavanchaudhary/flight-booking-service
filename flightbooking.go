8e48e561ecd5864c8b2342566a3de2ac15f195c1
pavanchaudhary <pavanchaudhary153@gmail.com>
2024-07-25 17:22:17 +0530

    added handler and functionality for booking flight

--- flightbooking.go
+++ flightbooking.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
+	"io"
+	"log"
 	"net/http"
 	"strconv"
 	"strings"
+	"sync"
 )
 
 type flightId string
@@ -59,7 +63,7 @@ func fetchFlights(w http.ResponseWriter, r *http.Request) {
 	}
 	rows, err := db.Query(flightQuery)
 	if err != nil {
-		http.Error(w, "Internal error", 342)
+		log.Println("Query failed for fetching flights")
 	}
 	defer rows.Close()
 	//Scan and append the responses
@@ -67,7 +71,7 @@ func fetchFlights(w http.ResponseWriter, r *http.Request) {
 	for rows.Next() {
 		var f FlightResponse
 		if err := rows.Scan(&f.FlightId, &f.Departure, &f.Arrival); err != nil {
-			http.Error(w, "sql scan error", 324)
+			log.Println("Error scanning db flight response")
 		}
 		fr = append(fr, f)
 	}
@@ -77,14 +81,158 @@ func fetchFlights(w http.ResponseWriter, r *http.Request) {
 	//Send responses as json
 	respData, err := json.Marshal(flightsList)
 	if err != nil {
-		fmt.Println("error marshalling JSON data")
+		log.Println("Error marshalling flight response JSON")
 	}
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(respData)
 }
 
+type gender string
+
+const (
+	male   gender = "male"
+	female gender = "female"
+	others gender = "others"
+)
+
+type passenger struct {
+	Age       int    `json:"age"`
+	FirstName string `json:"firstname"`
+	LastName  string `json:"lastname"`
+	Email     string `json:"email"`
+	Gender    gender `json:"gender"`
+}
+
+type FlightBookingRequest struct {
+	FlightId       flightId    `json:"flightId"`
+	Passengers     []passenger `json:"passengers"`
+	FlightDate     flightDate  `json:"date"`
+	Class          flightClass `json:"class"`
+	SeatsAvailable int         `json:"seatsavailable`
+}
+
+type flightTerminal string
+
+type FlightBookingReponse struct {
+	Pnr              string         `json:"pnr"`
+	Passengers       []passenger    `json:"passengers"`
+	Origin           airportCode    `json:"origin"`
+	Destination      airportCode    `json:"destination"`
+	FlightDate       flightDate     `json:"date"`
+	Class            flightClass    `json:"class"`
+	Departure        flightTime     `json:"departure"`
+	Arrival          flightTime     `json:"arrival"`
+	DepatureTerminal flightTerminal `json:"departure_terminal"`
+	ArrivalTerminal  flightTerminal `json:"arrival_terminal"`
+}
+
+func sendBookingDetails(w http.ResponseWriter, fbr *FlightBookingRequest, db *sql.DB, wg *sync.WaitGroup) {
+	//TODO: Add logic for generating PNR
+	//TODO: Add logic for assigning terminals and it's properties
+	defer wg.Done()
+	bookingResponse := FlightBookingReponse{
+		Pnr:              "TXQ173",
+		Passengers:       fbr.Passengers,
+		Origin:           "",
+		Destination:      "",
+		FlightDate:       fbr.FlightDate,
+		Class:            fbr.Class,
+		Departure:        "",
+		Arrival:          "",
+		DepatureTerminal: "T1",
+		ArrivalTerminal:  "T3",
+	}
+	rows, err := db.Query(fmt.Sprintf("SELECT flight.Origin, flight.Destination, flight.Departure, flight.Arrival FROM flight WHERE flightId = '%s'", fbr.FlightId))
+	if err != nil {
+		log.Println("Query failed for flight booking details")
+	}
+	defer rows.Close()
+	for rows.Next() {
+		if err := rows.Scan(&bookingResponse.Origin, &bookingResponse.Destination, &bookingResponse.Departure, &bookingResponse.Arrival); err != nil {
+			log.Println("Error scanning db flight booking info")
+		}
+	}
+	bookingResponseData, err := json.Marshal(bookingResponse)
+	if err != nil {
+		log.Println("Error marshalling booking response json")
+	}
+	w.Header().Set("Content-Type", "application/json")
+	_, err = w.Write(bookingResponseData)
+	if err != nil {
+		log.Println("Error sending HTTP response")
+	}
+	//TO-DO: push the booking details to DB
+}
+
+// Lock for booking seats.
+var queryLock *sync.Mutex = &sync.Mutex{}
+
+func bookFlights(w http.ResponseWriter, r *http.Request) {
+	var fbr FlightBookingRequest
+	reqbody, err := io.ReadAll(r.Body)
+	if err != nil {
+		log.Println("Failed to read http body for flight booking")
+	}
+
+	json.Unmarshal(reqbody, &fbr)
+	if err != nil {
+		log.Println("json unmarshalling failed for flight booking")
+	}
+	// Lock to prevent the http handler function return until go routines completes
+	// and no unnecessarily locking
+	wg := &sync.WaitGroup{}
+	db := GetMysqlConn()
+	if fbr.Class == economy {
+		//Lock the update seat query
+		queryLock.Lock()
+		rows, err := db.Query(fmt.Sprintf("SELECT flightSeatAvailability.SeatsAvailableEconomy FROM flight INNER JOIN flightSeatAvailability ON flight.FlightId = flightSeatAvailability.FlightId WHERE flightSeatAvailability.FlightDate = '%s' AND flight.EconomyClassAvailable = %t", fbr.FlightDate, true))
+		if err != nil {
+			log.Println("Query failed for flight booking details")
+		}
+		defer rows.Close()
+		for rows.Next() {
+			if err := rows.Scan(&fbr.SeatsAvailable); err != nil {
+				log.Println("Error scanning db flight booking response")
+			}
+		}
+		if fbr.SeatsAvailable > len(fbr.Passengers) {
+			_, err = db.Query(fmt.Sprintf("UPDATE flightSeatAvailability SET SeatsAvailableEconomy = %d WHERE FlightId = '%s' AND FlightDate = '%s'", fbr.SeatsAvailable-len(fbr.Passengers), fbr.FlightId, fbr.FlightDate))
+			if err != nil {
+				log.Println("Update Query failed for flight booking")
+			}
+			wg.Add(1)
+			go sendBookingDetails(w, &fbr, db, wg)
+		}
+		queryLock.Unlock()
+	} else {
+		//Lock the update seat query
+		queryLock.Lock()
+		rows, err := db.Query(fmt.Sprintf("SELECT flightSeatAvailability.SeatsAvailableBusiness FROM flight INNER JOIN flightSeatAvailability ON flight.FlightId = flightSeatAvailability.FlightId WHERE flightSeatAvailability.FlightDate = '%s' AND flight.BusinessClassAvailable = %t", fbr.FlightDate, true))
+		if err != nil {
+			log.Println("Query failed for flight booking details")
+		}
+		defer rows.Close()
+		for rows.Next() {
+			if err := rows.Scan(&fbr.SeatsAvailable); err != nil {
+				log.Println("Error scanning db flight booking response")
+			}
+		}
+		if fbr.SeatsAvailable > len(fbr.Passengers) {
+			_, err = db.Query(fmt.Sprintf("UPDATE flightSeatAvailability SET SeatsAvailableBusiness = %d WHERE FlightId = '%s' AND FlightDate = '%s'", fbr.SeatsAvailable-len(fbr.Passengers), fbr.FlightId, fbr.FlightDate))
+			if err != nil {
+				log.Println("Update Query failed for flight booking")
+			}
+			wg.Add(1)
+			go sendBookingDetails(w, &fbr, db, wg)
+		}
+		queryLock.Unlock()
+	}
+	wg.Wait()
+}
+
 func main() {
 	setConfig()
 	http.HandleFunc("/v1/flight/flights", fetchFlights)
+	http.HandleFunc("/v1/flight/book", bookFlights)
 	http.ListenAndServe("localhost:1729", nil)
 }
